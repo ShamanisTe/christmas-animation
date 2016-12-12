f05caa79c55f3e22c415a8c3274002877ff84573
KCH13111 <killian.charpentier@open-groupe.com>
2016-12-12 14:20:05 +0100

    add message for new year

--- animation1/text.js
+++ animation1/text.js
@@ -1,14 +1,14 @@
 (function(w){
     w.app = w.app || {};
     
+    var FRENCHKKINGCAKEMONTH = 0;
+
     var CHRISTMASMONTH = 11;
     var CHRISTMASDAY = 25;
+
     var ONEDAYTS = 1000 * 60 * 60 * 24;
 
-
-
     var calcDateBeforeChristmas = function(){
-        var now = new Date();
         var chritmas = new Date(now.getFullYear(), CHRISTMASMONTH, CHRISTMASDAY);
         var tsdiff = chritmas - now;
         var daydiff = Math.ceil(tsdiff / ONEDAYTS);
@@ -17,14 +17,29 @@
     };
 
     var initText = function(){
+        now = new Date();
+        if(now.getMonth() === FRENCHKKINGCAKEMONTH){
+            frenchKingCakeMessage();
+        } else {
+            christmasMessage();
+            
+        }
+
+    };
+
+    var frenchKingCakeMessage = function(){
+        var christmasTextEl = document.querySelector(".christmasText");
+        christmasTextEl.innerHTML = "Bonne année " + now.getFullYear() + " <br>et excellente galette des rois !";
+    };
+    var christmasMessage = function(){
         var diff = calcDateBeforeChristmas();
-        
+            
         if(diff > 0){
             var dateBeforeChristmasEl = document.querySelector(".date-before-christmas");
             dateBeforeChristmasEl.innerHTML = "J - " + calcDateBeforeChristmas();
         } else if(diff < 0){
             var christmasTextEl = document.querySelector(".christmasText");
-            christmasTextEl.innerHTML = "Joyeuses fêtes de fin d'année et <br>Bonne année " + (new Date().getFullYear() + 1)  + " !";
+            christmasTextEl.innerHTML = "Joyeuses fêtes de fin d'année et <br>Bonne année " + (now.getFullYear() + 1)  + " !";
         } else {
             var divs = document.querySelectorAll(".christmasText .christmasDayToRemove");
             for(var i = 0, c = divs.length; i<c;i++){
